3a524b5a6a6e9b7a79a8ba5ba1abd21f118f7d5d
Elizabeth Primeau <ella.j.primeau@gmail.com>
2024-09-29 15:56:21 -0400

    added histogram

--- DataSetAnalysis.py
+++ DataSetAnalysis.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
-data = pd.read_csv('train.csv', encoding='latin1')
-print(data.isnull().any())
-# print(data[data.isnull().any(axis=1)])
+url = 'https://raw.githubusercontent.com/ellaprimeau/CSI4106-a1/refs/heads/master/WineQT.csv'
+data = pd.read_csv(url, encoding='latin1')
 
+hist = data.hist()
+plt.show()
