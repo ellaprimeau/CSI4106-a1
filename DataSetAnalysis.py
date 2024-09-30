c10e275efc6cd62b044edfa58350ee69a6d35186
Elizabeth Primeau <ella.j.primeau@gmail.com>
2024-09-29 22:31:58 -0400

    added notebook

--- DataSetAnalysis.py
+++ DataSetAnalysis.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 from matplotlib import pyplot as plt
 
 url = 'https://raw.githubusercontent.com/ellaprimeau/CSI4106-a1/refs/heads/master/WineQT.csv'
 data = pd.read_csv(url, encoding='latin1')
 
-hist = data.hist()
+# data['chlorides'].hist()
+# plt.show()
+
+plt.hist(preprocessing.quantile_transform(data[['chlorides']], output_distribution="normal"))
 plt.show()
+# plt.plot(preprocessing.quantile_transform(data['chlorides']))
+# plt.show()
